3c8b16204350eecc054e297dcdb1a7aa62b60ba7
Filip Curkovic <filip.curkovic1@gmail.com>
2021-07-24 23:54:36 +0200

    Added starter angular & electron apps

---
+++ apps/electron/nx-cli/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'nx-cli';
+export const electronAppName = 'electron-nx-cli';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
